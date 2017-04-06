90e91a359f62b63bddc8df2b4b1c53271bf1ae81
superbigsea <29338383@qq.com>
2017-04-06 10:21:13 +0800

    fix bugs

--- zabbixwechat/urls.py
+++ zabbixwechat/urls.py
@@ -19,7 +19,6 @@ from zabbixwechat.all import all
 from zabbixwechat.multcomfirm import multcomfirm
 from zabbixwechat.singlecomfirm import singlecomfirm
 from zabbixwechat.getvalue import getvalue
-from zabbixwechat.boardcastsend import boardcastsend
 from zabbixwechat.detail import detail
 from zabbixwechat.report import report
 from zabbixwechat.tempclosealarm import tempclosealarm
@@ -34,7 +33,6 @@ urlpatterns = [
     url(r'^alarm/multcomfirm', multcomfirm),
     url(r'^alarm/singlecomfirm', singlecomfirm),
     url(r'^alarm/getvalue', getvalue),
-    url(r'^alarm/boardcastsend', boardcastsend),
     url(r'^alarm/detail', detail),
     url(r'^alarm/report', report),
     url(r'^alarm/checkdbnoresolved', checkdbnoresolved),
